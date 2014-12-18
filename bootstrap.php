35c31ec88327f302daab4c7ca4ca3ec1f71c17ec
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-18 18:38:50 +0000

    1.3.1 - reporting improvements + fixes
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-eu-vat-compliance/tags/1.3.1@1048342 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bootstrap.php
@@ -0,0 +1,307 @@
+<?php
+
+// Purpose boot-strap plugin
+
+if (!defined('ABSPATH')) die('Access denied.');
+
+if (class_exists('WC_EU_VAT_Compliance')) return;
+
+define('WC_EU_VAT_COMPLIANCE_DIR', dirname(__FILE__));
+
+$active_plugins = (array) get_option( 'active_plugins', array() );
+if (is_multisite()) $active_plugins = array_merge($active_plugins, get_site_option('active_sitewide_plugins', array()));
+
+if (!in_array('woocommerce/woocommerce.php', $active_plugins ) && !array_key_exists('woocommerce/woocommerce.php', $active_plugins)) return;
+
+// This plugin performs various distinct functions. So, we have separated the code accordingly.
+// Not all of these files may be present, depending on a) whether this is the free or premium version b) whether I've written the feature yet
+@include_once(WC_EU_VAT_COMPLIANCE_DIR.'/vat-number.php');
+@include_once(WC_EU_VAT_COMPLIANCE_DIR.'/reports.php');
+@include_once(WC_EU_VAT_COMPLIANCE_DIR.'/record-order-country.php');
+@include_once(WC_EU_VAT_COMPLIANCE_DIR.'/rates.php');
+@include_once(WC_EU_VAT_COMPLIANCE_DIR.'/premium.php');
+
+// Though the code is separated, some pieces are inter-dependent; the order also matters. So, don't assume you can just change this arbitrarily.
+$classes_to_activate = apply_filters('woocommerce_eu_vat_compliance_classes', array(
+	'WC_EU_VAT_Compliance',
+	'WC_EU_VAT_Compliance_VAT_Number',
+	'WC_EU_VAT_Compliance_Reports',
+	'WC_EU_VAT_Compliance_Record_Order_Country',
+	'WC_EU_VAT_Compliance_Rates',
+	'WC_EU_VAT_Compliance_Premium',
+));
+
+if (!class_exists('WC_EU_VAT_Compliance')):
+class WC_EU_VAT_Compliance {
+
+	private $default_vat_matches = 'VAT, V.A.T, IVA, I.V.A., Value Added Tax';
+	public $wc;
+
+	// Returns normalised data
+	public function get_vat_matches($format = 'array') {
+		$matches = get_option('woocommerce_eu_vat_compliance_vat_match', $this->default_vat_matches);
+		if (!is_string($matches) || empty($matches)) $matches = $this->default_vat_matches;
+		$arr = array_map('trim', explode(',', $matches));
+		if ('regex' == $format) {
+			$ret = '#(';
+			foreach ($arr as $str) {
+				$ret .= ($ret == '#(') ? preg_quote($str) : '|'.preg_quote($str);
+			}
+			$ret .= ')#i';
+			return $ret;
+		} elseif ('html-printable' == $format) {
+			$ret = '';
+			foreach ($arr as $str) {
+				$ret .= ($ret == '') ? htmlspecialchars($str) : ', '.htmlspecialchars($str);
+			}
+			return $ret;
+		} elseif ('sqlregex' == $format) {
+			$ret = '';
+			foreach ($arr as $str) {
+				$ret .= ($ret == '') ? esc_sql($str) : '|'.esc_sql($str);
+			}
+			return $ret;
+		}
+		return $arr;
+	}
+
+	public function __construct() {
+		add_action('plugins_loaded', array($this, 'plugins_loaded'));
+
+		add_action( 'woocommerce_settings_tax_options_end', array($this, 'woocommerce_settings_tax_options_end'));
+		add_action( 'woocommerce_update_options_tax', array( $this, 'woocommerce_update_options_tax'));
+
+		add_filter('network_admin_plugin_action_links', array($this, 'plugin_action_links'), 10, 2);
+		add_filter('plugin_action_links', array($this, 'plugin_action_links'), 10, 2);
+
+		add_option('woocommerce_eu_vat_compliance_vat_match', $this->default_vat_matches);
+
+		$this->settings = array(
+			array(
+				'name' 		=> __( 'Phrase matches used to identify VAT taxes', 'wc_eu_vat_compliance' ),
+				'desc' 		=> __( 'A comma-separated (optional spaces) list of strings (phrases) used to identify taxes which are EU VAT taxes. One of these strings must be used in your tax name labels (i.e. the names used in your tax tables) if you wish the tax to be identified as EU VAT.', 'wc_eu_vat_compliance' ),
+				'id' 		=> 'woocommerce_eu_vat_compliance_vat_match',
+				'type' 		=> 'text',
+				'default'		=> $this->default_vat_matches
+			)
+		);
+
+	}
+
+	public function get_order($order_id) {
+		if (function_exists('wc_get_order')) {
+			$order = wc_get_order($order_id);
+		} else {
+			$order = new WC_Order();
+			$get_order = $order->get_order($order_id);
+		}
+		return $order;
+	}
+
+	// Pass in a WC_Order object, or an order number
+	public function get_vat_paid($order, $allow_quick = false, $set_on_quick = false) {
+
+		if (!is_a($order, 'WC_Order') && is_numeric($order)) {
+			$order = $this->get_order($order);
+		}
+
+		$post_id = (isset($order->post)) ? $order->post->ID : $order->id;
+
+		if ($allow_quick) {
+			$vat_paid = get_post_meta($post_id, 'vat_compliance_vat_paid', true);
+			if (!empty($vat_paid)) {
+				return maybe_unserialize($vat_paid);
+			}
+		}
+
+		$taxes = $order->get_taxes();
+
+		if (!is_array($taxes)) return false;
+		if (empty($taxes)) return 0;
+
+		// Get an array of matches
+		$vat_strings = $this->get_vat_matches('regex');
+
+		// Not get_woocommerce_currency(), as currency switcher plugins filter that.
+		$base_currency = get_option('woocommerce_currency');
+		// WC 2.0 did not store order currencies.
+		$currency = method_exists($order, 'get_order_currency') ? $order->get_order_currency() : $base_currency;
+
+		$vat_total = 0;
+		$vat_shipping_total = 0;
+		$vat_total_base_currency = 0;
+		$vat_shipping_total_base_currency = 0;
+		$base_currency_totals_are_reliable = true;
+
+		foreach ($taxes as $tax) {
+			if (!is_array($tax) || !isset($tax['label'])) continue;
+			if (!preg_match($vat_strings, $tax['label'])) continue;
+
+			if (!empty($tax['tax_amount'])) $vat_total += $tax['tax_amount'];
+			if (!empty($tax['shipping_tax_amount'])) $vat_shipping_total += $tax['shipping_tax_amount'];
+
+
+			if ($currency != $base_currency) {
+				if (empty($tax['tax_amount_base_currency'])) {
+					// This will be wrong, of course, unless your conversion rate is 1:1
+					if (!empty($tax['tax_amount'])) $vat_total_base_currency += $tax['tax_amount'];
+					if (!empty($tax['shipping_tax_amount'])) $vat_shipping_total_base_currency += $tax['shipping_tax_amount'];
+					$base_currency_totals_are_reliable = false;
+				} else {
+					if (!empty($tax['tax_amount'])) $vat_total_base_currency += $tax['tax_amount_base_currency'];
+					if (!empty($tax['shipping_tax_amount'])) $vat_shipping_total_base_currency += $tax['shipping_tax_amount_base_currency'];
+				}
+			} else {
+				$vat_total_base_currency = $vat_total;
+				$vat_shipping_total_base_currency = $vat_shipping_total;
+			}
+
+		}
+
+		// We may as well return the kitchen sink, since we've spent the cycles on getting it.
+		$vat_paid = apply_filters('wc_eu_vat_compliance_get_vat_paid', array(
+			'items_total' => $vat_total,
+			'shipping_total' => $vat_shipping_total,
+			'total' => $vat_total + $vat_shipping_total,
+			'currency' => $currency,
+			'base_currency' => $base_currency,
+			'items_total_base_currency' => $vat_total_base_currency,
+			'shipping_total_base_currency' => $vat_shipping_total_base_currency,
+			'total_base_currency' => $vat_total_base_currency + $vat_shipping_total_base_currency,
+			'base_currency_totals_are_reliable' => $base_currency_totals_are_reliable
+		), $order, $taxes, $currency, $base_currency);
+
+/*
+e.g. (and remember, there may be other elements which are not VAT).
+
+Array
+(
+    [62] => Array
+        (
+            [name] => GB-VAT (UNITED KINGDOM)-1
+            [type] => tax
+            [item_meta] => Array
+                (
+                    [rate_id] => Array
+                        (
+                            [0] => 28
+                        )
+
+                    [label] => Array
+                        (
+                            [0] => VAT (United Kingdom)
+                        )
+
+                    [compound] => Array
+                        (
+                            [0] => 1
+                        )
+
+                    [tax_amount_base_currency] => Array
+                        (
+                            [0] => 2
+                        )
+
+                    [tax_amount] => Array
+                        (
+                            [0] => 3.134
+                        )
+
+                    [shipping_tax_amount_base_currency] => Array
+                        (
+                            [0] => 2.8
+                        )
+
+                    [shipping_tax_amount] => Array
+                        (
+                            [0] => 4.39
+                        )
+
+                )
+
+            [rate_id] => 28
+            [label] => VAT (United Kingdom)
+            [compound] => 1
+            [tax_amount_base_currency] => 2
+            [tax_amount] => 3.134
+            [shipping_tax_amount_base_currency] => 2.8
+            [shipping_tax_amount] => 4.39
+        )
+
+
+*/
+		if ($set_on_quick) {
+			update_post_meta($post_id, 'vat_compliance_vat_paid', apply_filters('wc_eu_vat_compliance_vat_paid', $vat_paid, $order));
+		}
+
+		return $vat_paid;
+
+	}
+
+
+	public function plugin_action_links($links, $file) {
+		if (is_array($links) && strpos($file, basename(WC_EU_VAT_COMPLIANCE_DIR).'/eu-vat-compliance') !== false) {
+			$page = (defined('WOOCOMMERCE_VERSION') && version_compare(WOOCOMMERCE_VERSION, '2.1', '<')) ? 'woocommerce_settings' : 'wc-settings';
+			$settings_link = '<a href="'.admin_url('admin.php').'?page='.$page.'&tab=tax">'.__("WooCommerce Tax Settings", "wc_eu_vat_compliance").'</a>';
+			array_unshift($links, $settings_link);
+			if (false === strpos($file, 'premium')) {
+				$settings_link = '<a href="https://www.simbahosting.co.uk/s3/product/woocommerce-eu-vat-compliance/">'.__("Premium Version", "wc_eu_vat_compliance").'</a>';
+				array_unshift($links, $settings_link);
+			}
+		}
+		return $links;
+	}
+
+	public function woocommerce_settings_tax_options_end() {
+		woocommerce_admin_fields($this->settings);
+	}
+
+	public function woocommerce_update_options_tax() {
+		woocommerce_update_options($this->settings);
+	}
+
+	// From WC 2.2
+	public function order_status_to_text($status) {
+		$order_statuses = array(
+			'wc-pending'    => _x( 'Pending Payment', 'Order status', 'woocommerce' ),
+			'wc-processing' => _x( 'Processing', 'Order status', 'woocommerce' ),
+			'wc-on-hold'    => _x( 'On Hold', 'Order status', 'woocommerce' ),
+			'wc-completed'  => _x( 'Completed', 'Order status', 'woocommerce' ),
+			'wc-cancelled'  => _x( 'Cancelled', 'Order status', 'woocommerce' ),
+			'wc-refunded'   => _x( 'Refunded', 'Order status', 'woocommerce' ),
+			'wc-failed'     => _x( 'Failed', 'Order status', 'woocommerce' ),
+		);
+		$order_statuses = apply_filters( 'wc_order_statuses', $order_statuses );
+
+		if ($status === true) return $order_statuses;
+
+		if (substr($status, 0, 3) != 'wc-') $status = 'wc-'.$status;
+		return (isset($order_statuses[$status])) ? $order_statuses[$status] : __('Unknown', 'wc_eu_vat_compliance').' ('.substr($status, 3).')';
+	}
+
+	public function plugins_loaded() {
+		load_plugin_textdomain('wc_eu_vat_compliance', false, WC_EU_VAT_COMPLIANCE_DIR.'/languages');
+		if (defined('WOOCOMMERCE_VERSION') && version_compare(WOOCOMMERCE_VERSION, '2.1', '<')) {
+			global $woocommerce;
+			$this->wc = $woocommerce;
+		} elseif (function_exists('WC')) {
+			$this->wc = WC();
+		}
+	}
+}
+endif;
+
+if (!function_exists('WooCommerce_EU_VAT_Compliance')):
+function WooCommerce_EU_VAT_Compliance($class = 'WC_EU_VAT_Compliance') {
+	global $woocommerce_eu_vat_compliance_classes;
+	return $woocommerce_eu_vat_compliance_classes[$class];
+}
+endif;
+
+global $woocommerce_eu_vat_compliance_classes;
+$woocommerce_eu_vat_compliance_classes = array();
+foreach ($classes_to_activate as $cl) {
+	if (class_exists($cl) && (empty($woocommerce_eu_vat_compliance_classes[$cl]))) {
+		$woocommerce_eu_vat_compliance_classes[$cl] = new $cl;
+	}
+}
