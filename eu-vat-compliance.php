df97ef8d4036265b9d4babcb3f0d554b8a9198a7
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-26 12:19:16 +0000

    1.5.3 - fix missing file
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-eu-vat-compliance/tags/1.5.3@1054466 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ eu-vat-compliance.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: WooCommerce EU VAT Compliance (Free)
+Plugin URI: https://www.simbahosting.co.uk/s3/product/woocommerce-eu-vat-compliance/
+Description: Provides features to assist WooCommerce with EU VAT compliance
+Version: 1.5.3
+Text Domain: wc_eu_vat_compliance
+Domain Path: /languages
+Author: David Anderson
+Author URI: http://www.simbahosting.co.uk/s3/shop/
+Requires at least: 3.1
+Tested up to: 4.1
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Copyright: 2014- David Anderson
+Portions licenced under the GPL v3 from other authors
+*/
+
+// The present file has minimal content to avoid needing to duplicate code changes in the free/premium versions (whose headers, above, can differ), whilst keeping both in the same source control
+
+if (!defined('ABSPATH')) die('Access denied.');
+require(dirname(__FILE__).'/bootstrap.php');
